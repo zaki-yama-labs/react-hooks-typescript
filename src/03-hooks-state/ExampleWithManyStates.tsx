0a438d9ce5e1c37b274fbd9a454358c2d475a52c
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-04-16 06:46:34 +0900

    03. ヒント：複数の state 変数を使う

---
+++ src/03-hooks-state/ExampleWithManyStates.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+export function ExampleWithManyStates() {
+  // Declare multiple state variables;
+  const [age, setAge] = useState(42);
+  const [fruit, setFruit] = useState('banana');
+  const [todos, setTodos] = useState({ text: 'Learn Hooks' });
+
+  return (
+    <div>
+      <p>You age: {age}</p>
+      <button onClick={() => setAge(age + 1)}>age++</button>
+      <p>Your favorite fruit:</p>
+      <select value={fruit} onChange={(e) => setFruit(e.target.value)}>
+        <option value="banana">バナナ</option>
+        <option value="apple">りんご</option>
+        <option value="orange">みかん</option>
+      </select>
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import { Example } from "./03-hooks-state/Example";
+import { ExampleWithManyStates } from "./03-hooks-state/ExampleWithManyStates";
 
-render(<Example />, document.getElementById('main'));
+render(<ExampleWithManyStates />, document.getElementById('main'));
