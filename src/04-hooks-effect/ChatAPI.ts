4d98b4166272aa2ddaa581c8b4acb31811ee4cda
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-02-23 17:27:00 +0900

    ChatAPIのダミー実装

---
+++ src/04-hooks-effect/ChatAPI.ts
@@ -0,0 +1,24 @@
+const fakeRequest = (request: string) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`${request} finished`);
+    }, 1000);
+  });
+};
+
+export class ChatAPI {
+
+  static async subscribeToFriendStatus(id: string, cb: Function) {
+    const _ = await fakeRequest('subscribe');
+    cb({
+      isOnline: true
+    });
+  }
+
+  static async unsubscribeFromFriendStatus(id: string, cb: Function) {
+    const _ = await fakeRequest('unsubscribe');
+    cb({
+      isOnline: true
+    });
+  }
+}
