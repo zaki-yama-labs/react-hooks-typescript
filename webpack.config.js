3a219a63bc577fec918ecfd1d347a2a38a26ae8d
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-02-23 17:15:46 +0900

    use webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
+const path = require('path');
+
 module.exports = {
   mode: "development",
 
   entry: "./src/index.tsx",
   output: {
-    path: `${__dirname}/dist`,
+    path: path.join(__dirname, '/dist'),
     filename: "index.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9000,
   },
   module: {
     rules: [
